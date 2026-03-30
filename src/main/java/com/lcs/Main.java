23b99ea3e1ea962764b51c6c1ac521012bb7bba4
Layne <Layne@atguigu.com>
2026-03-30 16:36:32 +0800

    hot-fix commit1

--- src/main/java/com/lcs/Main.java
+++ src/main/java/com/lcs/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.println("hello git!");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
+        System.out.println("hello git!4");
 
     }
 }
